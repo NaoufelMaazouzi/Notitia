126115fb99c697440c1241765f9fde92f6e3aea5
Naoufel <naoufel.maazouzi@live.fr>
2021-03-17 18:48:06 +0100

    commit for deploy on heroku

--- js/script.js
+++ js/script.js
@@ -1,20 +1,68 @@
-$(document).ready(function() {
-            $(".icone-menu").on("click", function() {
-                  $("nav ul").toggleClass("showing");
-            });
+$(document).ready(function () {
+      $(".icone-menu").on("click", function () {
+            $("nav ul").toggleClass("showing");
       });
+});
 
-      /* Scrolling Effect
 
-      $(window).on("scroll", function() {
-            if($(window).scrollTop()) {
-                  $('nav').addClass('black');
-            }
+function validation() {
+      var name = document.getElementById("name").value;
+      var email = document.getElementById("email").value;
+      var phone = document.getElementById("phone").value;
+      var message = document.getElementById("message").value;
 
-            else {
-                  $('nav').removeClass('black');
-            }
-      })*/
+      console.log("nom", name);
+
+      if (name !== "" && /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email) && message !== "" && phone !== "") {
+            Swal.fire({
+                  icon: 'success',
+                  title: 'Merci',
+                  text: 'Votre message a été envoyé !',
+                  showConfirmButton: false,
+            })
+      } else {
+            Swal.fire({
+                  icon: 'error',
+                  title: 'Oops...',
+                  text: 'Il faut remplir tous les champs et saisir une adresse email valide !',
+            })
+      }
+}
+
+function validationFooter() {
+      var name = document.getElementById("name2").value;
+      var email = document.getElementById("email2").value;
+      var phone = document.getElementById("phone2").value;
+
+      console.log("nom", name);
+
+      if (name !== "" && /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email) && phone !== "") {
+            Swal.fire({
+                  icon: 'success',
+                  title: 'Merci',
+                  text: 'Votre message a été envoyé !',
+                  showConfirmButton: false,
+            })
+      } else {
+            Swal.fire({
+                  icon: 'error',
+                  title: 'Oops...',
+                  text: 'Il faut remplir tous les champs et saisir une adresse email valide !',
+            })
+      }
+}
+
+/* Scrolling Effect
+
+$(window).on("scroll", function() {
+      if($(window).scrollTop()) {
+            $('nav').addClass('black');
+      }
+
+      else {
+            $('nav').removeClass('black');
+      }
+})*/
 
 /* let header = document.querySelector("nav")
 let minScrollValue = 15;
---
+++ php/contact-form-handle-footer.php
@@ -0,0 +1,19 @@
+<?php
+if(isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $visitor_phone = $_POST['phone'];
+
+    $email_from = $visitor_email;
+    $email_subject = "Nouveau message provenant du site Notitia!";
+    $email_body = "Nom de l'utilisateur: $name.\n".
+                    "Email de l'utilisateur: $visitor_email.\n".
+                        "Numéro de téléphone de l'utilisateur: $visitor_phone.\n".
+    $to = "naoufel.maazouzi@live.fr, wambersie.ines@hotmail.com, tiffainewillemin@gmail.com, anadarochafreire@gmail.com" ;
+    $headers = "From: $email_from \r\n";
+    $headers .= "Repondre à: $visitor_email \r\n";
+    mail($to,$email_subject,$email_body,$headers);
+    header("Location: https://notitia.000webhostapp.com/", sleep(3));
+}
+
+?>
